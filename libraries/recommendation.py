60f778fce4287da50625988981b678a069c11ea0
Taweechai Yuenyang <krumii.it@gmail.com>
2022-02-10 16:57:12 +0700

    new trview

---
+++ libraries/recommendation.py
@@ -0,0 +1,184 @@
+from tradingview_ta import TA_Handler
+
+
+class TimeInterval:
+    def __init__(self):
+        self.INTERVAL_1_MINUTE = "1m"
+        self.INTERVAL_5_MINUTES = "5m"
+        self.INTERVAL_15_MINUTES = "15m"
+        self.INTERVAL_30_MINUTES = "30m"
+        self.INTERVAL_1_HOUR = "1h"
+        self.INTERVAL_2_HOURS = "2h"
+        self.INTERVAL_4_HOURS = "4h"
+        self.INTERVAL_1_DAY = "1d"
+        self.INTERVAL_1_WEEK = "1W"
+        self.INTERVAL_1_MONTH = "1M"
+
+    def get_interval(self):
+        list_of_intervals = [
+            # self.INTERVAL_30_MINUTES,
+            # self.INTERVAL_1_HOUR,
+            self.INTERVAL_2_HOURS,
+            self.INTERVAL_4_HOURS,
+            self.INTERVAL_1_DAY,
+            self.INTERVAL_1_WEEK,
+            self.INTERVAL_1_MONTH,
+        ]
+
+        return list_of_intervals
+
+    def get_thai_language(self, time_interval):
+        if time_interval == self.INTERVAL_1_MINUTE:
+            return '1นาที'
+
+        elif time_interval == self.INTERVAL_5_MINUTES:
+            return '5นาที'
+
+        elif time_interval == self.INTERVAL_15_MINUTES:
+            return '15นาที'
+
+        elif time_interval == self.INTERVAL_30_MINUTES:
+            return '30นาที'
+
+        elif time_interval == self.INTERVAL_1_HOUR:
+            return '1ชั่วโมง'
+
+        elif time_interval == self.INTERVAL_2_HOURS:
+            return '2ชั่วโมง'
+
+        elif time_interval == self.INTERVAL_4_HOURS:
+            return '4ชั่วโมง'
+
+        elif time_interval == self.INTERVAL_1_DAY:
+            return '1วัน'
+
+        elif time_interval == self.INTERVAL_1_WEEK:
+            return '1สัปดาห์'
+
+        elif time_interval == self.INTERVAL_1_MONTH:
+            return '1เดือน'
+
+
+class Recommendation:
+    def __init__(self):
+        print(f"*******************************")
+        print(f"🤫 starting get recommendation.")
+        print(f"*******************************")
+        self.__VERSION__ = '0.1b'
+        self.__DATA__INIT__ = {
+            'SYMBOL': "",
+            'CURRENCY': "",
+            'EXCHANGE': "",
+            'RECOMMENDATION': "",
+            'BUY': 0,
+            'SELL': 0,
+            'NEUTRAL': "",
+            'TIMEFRAME': "",
+            'VERSION': self.__VERSION__
+        }
+
+    def summary(self, exchange='BITKUB', symbol='BTC', currency='THB', time=TimeInterval().INTERVAL_4_HOURS):
+        try:
+            obj = TA_Handler(
+                symbol=f'{symbol}{currency}',
+                screener='crypto',
+                exchange=exchange,
+                interval=time
+            )
+            data = obj.get_analysis().summary
+            if len(data) <= 0:
+                return self.__DATA__INIT__
+
+            obj = {
+                'SYMBOL': symbol,
+                'CURRENCY': currency,
+                'EXCHANGE': exchange,
+                'RECOMMENDATION': data['RECOMMENDATION'],
+                'BUY': data['BUY'],
+                'SELL': data['SELL'],
+                'NEUTRAL': data['NEUTRAL'],
+                'TIMEFRAME': time,
+                'VERSION': self.__VERSION__
+            }
+
+            # print(obj)
+            return obj
+
+        except Exception as e:
+            self.__DATA__INIT__['error'] = e
+            return self.__DATA__INIT__
+
+    def oscillators(self, exchange='Bitkub', symbol='BTC', currency='THB', time=TimeInterval().INTERVAL_1_DAY):
+        try:
+            obj = TA_Handler(
+                symbol=f'{symbol}{currency}',
+                screener='crypto',
+                exchange=exchange,
+                interval=time
+            )
+            data = obj.get_analysis().oscillators
+            if len(data) <= 0:
+                return self.__DATA__INIT__
+
+            self.__DATA__INIT__['SYMBOL'] = symbol
+            self.__DATA__INIT__['CURRENCY'] = currency
+            self.__DATA__INIT__['EXCHANGE'] = exchange
+            self.__DATA__INIT__['TIMEFRAME'] = time
+            self.__DATA__INIT__['COMPUTE'] = data['COMPUTE']
+            self.__DATA__INIT__['VERSION'] = self.__VERSION__
+            print(data)
+            return data
+
+        except Exception as e:
+            self.__DATA__INIT__['error'] = e
+            return self.__DATA__INIT__
+
+    def moving_averages(self, exchange='Bitkub', symbol='BTC', currency='THB', time=TimeInterval().INTERVAL_1_DAY):
+        try:
+            obj = TA_Handler(
+                symbol=f'{symbol}{currency}',
+                screener='crypto',
+                exchange=exchange,
+                interval=time
+            )
+            data = obj.get_analysis().moving_averages
+            if len(data) <= 0:
+                return self.__DATA__INIT__
+
+            self.__DATA__INIT__['SYMBOL'] = symbol
+            self.__DATA__INIT__['CURRENCY'] = currency
+            self.__DATA__INIT__['EXCHANGE'] = exchange
+            self.__DATA__INIT__['TIMEFRAME'] = time
+            self.__DATA__INIT__['COMPUTE'] = data['COMPUTE']
+            self.__DATA__INIT__['VERSION'] = self.__VERSION__
+            print(self.__DATA__INIT__)
+            return data
+
+        except Exception as e:
+            self.__DATA__INIT__['error'] = e
+            return self.__DATA__INIT__
+
+    def indicators(self, exchange='Bitkub', symbol='BTC', currency='THB', time=TimeInterval().INTERVAL_1_DAY):
+        try:
+            obj = TA_Handler(
+                symbol=f'{symbol}{currency}',
+                screener='crypto',
+                exchange=exchange,
+                interval=time
+            )
+            data = obj.get_analysis().indicators["RSI"]
+            if len(data) <= 0:
+                return self.__DATA__INIT__
+
+            self.__DATA__INIT__['SYMBOL'] = symbol
+            self.__DATA__INIT__['CURRENCY'] = currency
+            self.__DATA__INIT__['EXCHANGE'] = exchange
+            self.__DATA__INIT__['TIMEFRAME'] = time
+            self.__DATA__INIT__['COMPUTE'] = data['COMPUTE']
+            self.__DATA__INIT__['VERSION'] = self.__VERSION__
+            print(self.__DATA__INIT__)
+            return data
+
+        except Exception as e:
+            self.__DATA__INIT__['error'] = e
+            return self.__DATA__INIT__
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from binance.client import Client
 from binance.exceptions import BinanceAPIException
 from libraries.exports import Exports
+from libraries.recommendation import Recommendation
 from dotenv import load_dotenv
 
 # init
@@ -11,49 +12,52 @@ api_key = os.environ.get('BINANCE_API_KEY')
 api_secret = os.environ.get('BINANCE_API_SECRET')
 
 export = Exports()
+recommendation = Recommendation()
 
 
 def main():
     client = Client(api_key, api_secret)
 
-    # get account
-    bal = []
-    balance = client.get_account()
-    i = 0
-    while i < len(balance['balances']):
-        r = balance['balances'][i]
-        if str(r['asset']).find('LD') < 0 and float(str(r['free'])) > 0:
-            print(f"{(i + 1)}. ==> {r}")
-            bal.append(r)
-
-        i += 1
-    export.export_to_json('exports/balance.json', bal)
-
-
-    # get all coin
-    bal = []
-    info = client.get_account_snapshot(type='SPOT')
-    snapshotVos = info['snapshotVos']
-    i = 0
-    while i < len(snapshotVos):
-        assets = snapshotVos[i]['data']['balances']
-        for r in assets:
-            print(r)
-            if r['asset'].find('LD') < 0:
-                print(r)
-                bal.append(r)
-
-        i += 1
-    export.export_to_json('exports/assets.json', bal)
-
-    # # get products
-    # products = client.get_products()
+    # # get account
+    # bal = []
+    # balance = client.get_account()
     # i = 0
-    # while i < len(products['data']):
-    #     r = products['data'][i]
-    #     print(f"{(i + 1)}. {r['b']}/{r['q']}")
-    #     i += 1
+    # while i < len(balance['balances']):
+    #     r = balance['balances'][i]
+    #     if str(r['asset']).find('LD') < 0 and float(str(r['free'])) > 0:
+    #         print(f"{(i + 1)}. ==> {r}")
+    #         bal.append(r)
     #
+    #     i += 1
+    # export.export_to_json('exports/balance.json', bal)
+    #
+    #
+    # # get all coin
+    # bal = []
+    # info = client.get_account_snapshot(type='SPOT')
+    # snapshotVos = info['snapshotVos']
+    # i = 0
+    # while i < len(snapshotVos):
+    #     assets = snapshotVos[i]['data']['balances']
+    #     for r in assets:
+    #         print(r)
+    #         if r['asset'].find('LD') < 0:
+    #             print(r)
+    #             bal.append(r)
+    #
+    #     i += 1
+    # export.export_to_json('exports/assets.json', bal)
+
+    # get products
+    products = client.get_products()
+    i = 0
+    while i < len(products['data']):
+        r = products['data'][i]
+        print(f"{(i + 1)}. {r['b']}/{r['q']}")
+        data = recommendation.summary(exchange='BINANCE', symbol=str(r['b']),currency=str(r['q']))
+        print(data)
+        i += 1
+
     client.close_connection()
 
 
